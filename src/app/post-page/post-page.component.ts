c5de41ab22ebf69966fa0b4aa5b79a25fe866b37
denvysotskyi <denvysotskyi@gmail.com>
2021-11-09 09:37:12 +0200

    Modify post-page

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+import { PostService } from '../shared/post.service';
+import { IPost } from '../shared/interfaces';
 
 @Component({
   selector: 'app-post-page',
@@ -7,9 +13,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostPageComponent implements OnInit {
 
-  constructor() { }
+  post$?: Observable<IPost>
+
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService
+  ) {}
 
   ngOnInit(): void {
+    this.post$ = this.route.params
+      .pipe(
+        switchMap((params: Params) => {
+          return this.postService.getById(params['id'])
+        })
+      )
   }
 
 }
