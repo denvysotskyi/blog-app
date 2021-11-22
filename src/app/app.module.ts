bcfa317576d35cfed3ea9ceff0bc095da7866fd6
denvysotskyi <denvysotskyi@gmail.com>
2021-11-22 16:28:54 +0200

    Modify interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,19 @@ import { NgModule, Provider } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { HTTP_INTERCEPTORS } from '@angular/common/http'
 import { registerLocaleData } from '@angular/common'
+import { ServiceWorkerModule } from '@angular/service-worker'
 import uaLocale from '@angular/common/locales/ru-UA'
 
-import { environment } from '../environments/environment'
 import { AppComponent } from './app.component'
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component'
 import { HomePageComponent } from './home-page/home-page.component'
 import { PostPageComponent } from './post-page/post-page.component'
 import { PostComponent } from './shared/components/post/post.component'
 import { AppRoutingModule } from './app-routing.module'
-import { SharedModule } from './shared/shared.module'
-import { ServiceWorkerModule } from '@angular/service-worker'
-import { AuthInterceptor } from './shared/auth.interceptor'
 import { ErrorPageComponent } from './error-page/error-page.component'
+import { SharedModule } from './shared/shared.module'
+import { AuthInterceptor } from './shared/auth.interceptor'
+import { environment } from '../environments/environment'
 
 registerLocaleData(uaLocale, 'ua')
 
